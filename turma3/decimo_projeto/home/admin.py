0e18c41501f93827d11322a7b92cc863d2a286e0
Paulo Junior <npaulo.ce@gmail.com>
2024-01-18 18:40:11 -0300

    Aula DJANGO implementando o Nutrion 18/01

--- turma3/decimo_projeto/home/admin.py
+++ turma3/decimo_projeto/home/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Produto
 
 class ProdutoList(admin.ModelAdmin):
-    list_display = ('nome_produto', 'preco', 'imagem_produto', 'descricao')
+    list_display = ('nome_produto', 'imagem_produto', 'descricao')
 
 admin.site.register(Produto, ProdutoList)
