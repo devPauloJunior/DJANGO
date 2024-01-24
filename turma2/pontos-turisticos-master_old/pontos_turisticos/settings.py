d40c466a585a24b4955ae8099cb86489a78082bf
Paulo Junior <npaulo.ce@gmail.com>
2024-01-24 19:21:45 -0300

    Aula DJANGO + REST pontos turisticos 24/01

--- turma2/pontos-turisticos-master_old/pontos_turisticos/settings.py
+++ turma2/pontos-turisticos-master_old/pontos_turisticos/settings.py
@@ -81,10 +81,20 @@ WSGI_APPLICATION = 'pontos_turisticos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-from dj_database_url import parse as dburl
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+# from dj_database_url import parse as dburl
+# default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+# DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pontos_turisticos',
+        'USER': 'user_con',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
